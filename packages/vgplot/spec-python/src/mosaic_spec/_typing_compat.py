b849edd79ae4a1cdd5f4b8f64ca2182552278e98
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2026-07-28 16:53:14 +0000

    fix: inverse of what i wanted

--- packages/vgplot/spec-python/src/mosaic_spec/_typing_compat.py
+++ packages/vgplot/spec-python/src/mosaic_spec/_typing_compat.py
@@ -5,8 +5,8 @@ from __future__ import annotations
 import sys
 
 if sys.version_info >= (3, 15):
-    from typing_extensions import NotRequired, Required, TypeAliasType, TypedDict, TypeVar
-else:
     from typing import NotRequired, Required, TypeAliasType, TypedDict, TypeVar
+else:
+    from typing_extensions import NotRequired, Required, TypeAliasType, TypedDict, TypeVar
 
 __all__ = ("NotRequired", "Required", "TypeAliasType", "TypeVar", "TypedDict")
