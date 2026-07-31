0c5cb394581880c914068c23d6b38d5e372a9db3
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2026-07-31 18:26:32 +0000

    docs: Explain `Spec` woes

--- packages/vgplot/spec-python/tools/models/source.py
+++ packages/vgplot/spec-python/tools/models/source.py
@@ -1,11 +1,57 @@
 """[msgspec](https://msgspec.dev/) models for translating the mosaic schema.
 
-## Notes
-- Defines a greatly-reduced subset of JSON schema
-- Structs act **both** as filters (undeclared fields are skipped)
-  and validators (unexpected types within them raise during serde).
-- `forbid_unknown_fields=True` can be used to determine if it is safe to remove a field,
-  or to check a new schema hasn't introduced something unhandled
+# Spec
+[Spec]: https://github.com/uwdata/mosaic/blob/e3cb141cfae4a0692990a924c03772956af89f78/packages/vgplot/spec/src/spec/Spec.ts#L34-L70
+
+The current definition of [Spec] does not translate well through JSON schema to `TypedDict`.
+
+## What are `Spec` and `Component`?
+Here's a visual to illustrate the complexity
+
+```ts
+export type PlotMark = Area | ... | Density | DensityX | DensityY;
+//                     ----       ^           --------   --------
+//                     |        Union         |          |
+//                     Interface              |          |
+//                                            Interface(s) intersected with a
+//                                            union of 4x anonymous interfaces
+
+export type Component = HConcat | ... | Legend | PlotMark;
+//                     --------       ^          --------
+//                     |            Union        |
+//                     Interface                 Union (65 members)
+
+export type Spec = SpecHead & Component;
+//                 -------- ^ ---------|
+//                 |   Intersection    |
+//                 |                   Union (11 members)
+//                 Interface
+```
+
+## How does that fit into python?
+[Intersection Types]: https://www.typescriptlang.org/docs/handbook/2/objects.html#intersection-types
+
+A **limited form** of [Intersection Types] *are* representable using either `TypedDict` and `Protocol`.
+
+```py
+class SpecHead(TypedDict): ...
+class HConcat(TypedDict): ...
+class Area(TypedDict): ...
+
+class SpecHConcat(SpecHead, HConcat): ...
+class SpecArea(SpecHead, Area): ...
+
+type Spec = SpecHConcat | ... | SpecArea
+```
+
+Sadly, this doesn't cover intersecting with a union.
+
+## The fix?
+- Find all the deeply-nested union members (80-90ish)
+    - Includes defining names for those that are anonymous in the schema
+- Mix each one with `SpecHead` to create a new TypedDict
+    - Revealed issues with intersections allowing `Required & Not Required`
+- Union each of those to define `Spec`
 """
 
 from collections import deque
@@ -61,7 +107,15 @@ class ItemSchema(_RecursePropsUnionBase, forbid_unknown_fields=True):
 class JsonSchema(_RecursePropsUnionBase, forbid_unknown_fields=True):
     """The (useful) subset of JSON schema used in `"definitions"`.
 
-    Excludes `"$schema"` from all `Spec` members, while leaving a single version at the top-level.
+    ## Notes
+    - Defines a greatly-reduced subset of JSON schema
+        - Excludes `"$schema"` from all `Spec` members, while leaving a single version at the top-level.
+        - Exludes all unused keywords
+        - Narrows some types of the remaining parts
+    - Structs act **both** as filters (undeclared fields are skipped)
+      and validators (unexpected types within them raise during serde).
+    - `forbid_unknown_fields=True` can be used to determine if it is safe to remove a field,
+      or to check a new schema hasn't introduced something unhandled
     """
 
     type: Primitive | Sequence[Primitive] = field(default_factory=list)  # pyright: ignore[reportIncompatibleVariableOverride]
