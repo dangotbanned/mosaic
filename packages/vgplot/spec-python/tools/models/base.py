31cb76fe175f0f5322a55f1334d326bd4ac4f5fd
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2026-07-31 18:45:40 +0000

    chore: add extra typing to msgspec

--- packages/vgplot/spec-python/tools/models/base.py
+++ packages/vgplot/spec-python/tools/models/base.py
@@ -1,7 +1,18 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING, Any, ClassVar, Self
+
 import msgspec
 
 
 class Struct(msgspec.Struct, omit_defaults=True, repr_omit_defaults=True):
     """`omit_defaults=True, repr_omit_defaults=True`."""
+
+    if TYPE_CHECKING:
+        # NOTE: All are defined at runtime and documented
+        # Some aren't in the typing but all are missing for `ty`
+        __slots__ = ()
+        __struct_defaults__: ClassVar[tuple[Any, ...]]
+        __struct_encode_fields__: ClassVar[tuple[str, ...]]
+
+        def __copy__(self) -> Self: ...
