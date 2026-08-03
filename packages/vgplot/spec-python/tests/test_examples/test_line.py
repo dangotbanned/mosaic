30fc5c6725606ded9777cea0edffa05b776e866c
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2026-08-03 22:19:42 +0000

    tests: add more variants of line

--- packages/vgplot/spec-python/tests/test_examples/test_line.py
+++ packages/vgplot/spec-python/tests/test_examples/test_line.py
@@ -1,17 +1,51 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import Literal as L
 
-if TYPE_CHECKING:
-    import mosaic_spec as ms
+import pytest
+
+import mosaic_spec as ms
 
 
-def test_line() -> None:
+@pytest.fixture
+def file() -> str:
+    return "data/stocks.parquet"
+
+
+@pytest.fixture
+def mark() -> L["lineY"]:
+    return "lineY"
+
+
+@pytest.fixture
+def where() -> str:
+    return "Symbol = 'AAPL'"
+
+
+def test_infer(file: str, where: str, mark: L["lineY"]) -> None:
     _spec: ms.Spec = {
-        "data": {
-            "aapl": {"type": "parquet", "file": "data/stocks.parquet", "where": "Symbol = 'AAPL'"}
-        },
-        "plot": [{"mark": "lineY", "data": {"source": "aapl"}, "x": "Date", "y": "Close"}],
+        "data": {"aapl": {"type": "parquet", "file": file, "where": where}},
+        "plot": [{"mark": mark, "data": {"source": "aapl"}, "x": "Date", "y": "Close"}],
         "width": 680,
         "height": 200,
+    }
+
+
+def test_typed_dict(file: str, where: str, mark: L["lineY"]) -> None:
+    _spec = ms.SpecPlot(
+        data={"aapl": ms.DataParquet(type="parquet", file=file, where=where)},
+        plot=(ms.LineY(mark=mark, data={"source": "aapl"}, x="Date", y="Close"),),
+        width=680,
+        height=200,
+    )
+
+
+def test_invalid(file: str, where: str, mark: L["lineY"]) -> None:
+    _spec: ms.SpecPlot = {  # pyright: ignore[reportAssignmentType]
+        "data": {
+            "aapl": {"type": "parquet", "file": file, "where": where},
+            "plot": [{"mark": mark, "data": {"source": "aapl"}, "x": bytes(1), "y": "Close"}],
+            "width": 680,
+            "height": 200,
+        }  # ty: ignore[invalid-argument-type]
     }
