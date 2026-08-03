391a8ef4548c2978223cfe8027d368f605c133ec
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2026-08-03 18:27:51 +0000

    refactor: tidy up `typed_dict`

--- packages/vgplot/spec-python/tools/codegen/typed_dict.py
+++ packages/vgplot/spec-python/tools/codegen/typed_dict.py
@@ -1,48 +1,22 @@
-"""Building on top of generated `TypedDict`s.
-
-## Tasks
-- [ ] `Spec = SpecHead & Component`
-"""
+"""Building on top of generated `TypedDict`s."""
 
 from __future__ import annotations
 
 import dataclasses
 import enum
-from typing import TYPE_CHECKING, Final, Literal as L, NewType, assert_never
-
-# TODO @dangotbanned: Remove alias after `"pyright>1.1.411"` updates `typing_extensions` stubs
-from typing_extensions import Sentinel as sentinel  # ruff: ignore[camelcase-imported-as-lowercase]
+from itertools import chain
+from typing import TYPE_CHECKING, Final, Literal as L, assert_never
 
 from tools.codegen.docstrings import doc
 
 if TYPE_CHECKING:
-    from collections.abc import Iterator
+    from collections.abc import Iterator, Sequence
 
-PYRIGHT: Final = False
-"""In pyroject.toml, this is `tool.pyright.defineConstant = { "PYRIGHT" = true }`"""
-
-if PYRIGHT:
-    # NOTE: `Final` is a workaround for https://github.com/microsoft/pyright/issues/10744#issuecomment-5133597898
-    # but this breaks `sentinel` for `ty`
-    NoTotal: Final = sentinel("NoTotal")
-    NoClosed: Final = sentinel("NoClosed")
-    NoExtraItems: Final = sentinel("NoExtraItems")
-else:
-    NoTotal = sentinel("NoTotal")
-    NoClosed = sentinel("NoClosed")
-    NoExtraItems = sentinel("NoExtraItems")
 
 type TypeExpr = str
 """A string encoding a [type expression][1].
 
 [1]: https://typing.python.org/en/latest/spec/annotations.html#type-and-annotation-expressions
-"""
-
-RuntimeTypeExpr = NewType("RuntimeTypeExpr", str)
-"""A [type expression][1] that must be valid outside of [Annotation scopes][2].
-
-[1]: https://typing.python.org/en/latest/spec/annotations.html#type-and-annotation-expressions
-[2]: https://docs.python.org/3/reference/executionmodel.html#annotation-scopes
 """
 
 type Line = str
@@ -60,61 +34,50 @@ class Qualifier(enum.Enum):
     NOT_REQUIRED = "NotRequired[{0}]"
     """Use this when `total` isn't provided/more than half are required"""
 
+    @classmethod
+    def from_fields(cls, fields: Sequence[Field], /) -> Qualifier:
+        n = len(fields)
+        n_required = sum(fld.required for fld in fields)
+        if not n_required or n_required <= (n / 2):
+            return Qualifier.REQUIRED
+        if n_required != n:
+            return Qualifier.NOT_REQUIRED
+        return Qualifier.DEFAULT
+
 
 INDENT: Final = 4 * " "
+
+
+def _join_keywords(kwds: dict[str, str | bool | None]) -> str:
+    """Drop everything that is a default."""
+    return ", ".join(f"{k}={v}" for k, v in kwds.items() if v is not None)
 
 
 def iter_lines(
     name: str,
     fields: tuple[Field, ...] = (),
     *,
-    bases: tuple[str, ...] = (),
-    total: L[False] | NoTotal = NoTotal,
-    closed: bool | NoClosed = NoClosed,
-    extra_items: RuntimeTypeExpr | NoExtraItems = NoExtraItems,
+    bases: tuple[str, ...] = ("TypedDict",),
+    total: L[False] | None = None,
+    closed: L[True] | None = None,
+    extra_items: TypeExpr | None = None,
 ) -> Iterator[str]:
-    # NOTE: Will need to report what has been imported through the duration of writing a module
-    bases = bases or ("TypedDict",)
-    kwds: dict[str, str | bool] = (
-        {} if extra_items is NoExtraItems else {"extra_items": extra_items}
-    )
-
-    if closed is not NoClosed:
-        if kwds:
-            msg = f"Cannot combine closed={closed!r} and extra_items"
-            raise TypeError(msg)
-        kwds["closed"] = closed
-
-    if total is not NoTotal:
-        kwds["total"] = total
+    if closed and extra_items:
+        msg = f"Cannot combine closed={closed!r} and extra_items={extra_items!r}"
+        raise TypeError(msg)
+    kwds = {"total": total, "closed": closed, "extra_items": extra_items}
+    if not fields:
+        yield f"class {name}({', '.join(bases)}, {_join_keywords(kwds)}): ..."
+        return
+    if total is False:
         non_default = Qualifier.REQUIRED
     else:
-        non_default = Qualifier.DEFAULT
-
-    if fields:
-        if total is NoTotal:
-            n = len(fields)
-            n_required = sum(fld.required for fld in fields)
-            if not n_required or n_required <= (n / 2):
-                kwds["total"] = False
-                non_default = Qualifier.REQUIRED
-            elif n_required == n:
-                non_default = Qualifier.DEFAULT
-            else:
-                # NOTE: Might be lucky enough to not hit this
-                non_default = Qualifier.NOT_REQUIRED
-
-        kwds_str = ", ".join(f"{k}={v}" for k, v in kwds.items())
-        yield f"class {name}({', '.join(bases)}, {kwds_str}):"
-
-        # TODO @dangotbanned: Put the field types somewhere for import deps
-        for field in fields:
-            for line in field.iter_lines(non_default):
-                yield f"{INDENT}{line}"
-
-    else:
-        kwds_str = ", ".join(f"{k}={v}" for k, v in kwds.items())
-        yield f"class {name}({', '.join(bases)}, {kwds_str}): ..."
+        non_default = Qualifier.from_fields(fields)
+        if non_default is Qualifier.REQUIRED:
+            kwds["total"] = False
+    yield f"class {name}({', '.join(bases)}, {_join_keywords(kwds)}):"
+    for line in chain.from_iterable(fld.iter_lines(non_default) for fld in fields):
+        yield f"{INDENT}{line}"
 
 
 Q = Qualifier
