1db7b3c4a321f88df2e084967e9d2a2fdd00cb9b
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2026-08-18 13:49:48 +0000

    refactor: add base class, split out `NamedSequence`

--- packages/vgplot/spec-python/tools/models/json_wrapper.py
+++ packages/vgplot/spec-python/tools/models/json_wrapper.py
@@ -5,9 +5,9 @@
 """
 
 import collections.abc as cabc
-from typing import Any, Literal as L, final
+from typing import Any, Literal as L, Self, final
 
-from msgspec.structs import asdict as _asdict
+import msgspec
 
 from tools.models import base
 from tools.models.mosaic import DefName, JsonSchema
@@ -19,90 +19,150 @@ class _Tagged(base.Struct, tag=True, tag_field="tag"): ...
 
 _EMPTY_SCHEMA = JsonSchema()
 
-type JsonWrapper = (
-    Reference
-    | Union
-    | Literal
-    | Primitive
-    | PrimitiveUnion
-    | EmptySequence
-    | Sequence
-    | Object
-    | Unknown
-)
+
+# NOTE: Use `schema` last in the constructor and it will show last in the repr
+class JsonWrapper(_Tagged, kw_only=True):
+    schema: JsonSchema
+
+    @property
+    def description(self) -> str:
+        return self.schema.description
+
+    @classmethod
+    def from_schema(cls, schema: JsonSchema) -> Self:
+        """Wrap a json schema, making this layer a tagged union."""
+        msg = f"{cls.from_schema.__qualname__}() is not yet implemented"
+        raise NotImplementedError(msg)
+
+
+class _DirectWrapper(JsonWrapper):
+    """Doesn't collect any fields yet.
+
+    ## Important
+    As soon as a subclass does anything beyond being a tag,
+    they should switch to `JsonWrapper` and implement `from_schema` properly.
+    """
+
+    @classmethod
+    def from_schema(cls, schema: JsonSchema) -> Self:
+        return cls(schema=schema)
 
 
 @final
-class Unknown(_Tagged):
-    # `"items": {}, "type": "array"`
-    # the inner part is `Any`
-    schema: JsonSchema
+class Unknown(JsonWrapper):
+    """A schema that does not define validation constraints.
+
+    ## Examples
+
+    The elements in the array allow `Any`:
+    ```py
+    {"type": "array", "items": {}, "description": "An array of inline data values to visualize."}
+    ```
+
+    The object has `extra_items=Any`, in addition to the defined properties.
+    ```py
+    {
+        "type": "object",
+        "properties": {...},
+        "addditionalProperties": True,
+        "description": "Configuration options.",
+    }
+    ```
+    """
+
+    schema: JsonSchema = msgspec.field(default_factory=JsonSchema)
+
+    @classmethod
+    def from_schema(cls, schema: JsonSchema) -> Unknown:
+        if schema == _EMPTY_SCHEMA:
+            return Unknown()
+        # quite a hassle to remove defaults, leaving just description
+        raw = convert_json(schema, dict[str, Any])
+        if raw.keys() == {"description"}:
+            return Unknown(schema=schema)
+        msg = f"Unexpected schema pattern found: {schema!r}\n{raw!r}"
+        raise NotImplementedError(msg)
 
 
 @final
-class Reference(_Tagged):
-    # ref
-    schema: JsonSchema
+class Reference(JsonWrapper):
+    """`$ref`."""
+
+    ref: str
+
+    @classmethod
+    def from_schema(cls, schema: JsonSchema) -> Reference:
+        return Reference(ref=schema.ref, schema=schema)
 
 
 @final
-class Union(_Tagged):
-    # any_of
-    schema: JsonSchema
+class Union(JsonWrapper):
+    """`anyOf`."""
+
     members: cabc.Sequence[JsonWrapper]
 
     @classmethod
     def from_schema(cls, schema: JsonSchema) -> Union:
-        return Union(schema, [_from_schema(m) for m in schema.any_of])
+        return Union(members=[_from_schema(m) for m in schema.any_of], schema=schema)
 
 
 @final
-class Literal(_Tagged):
-    # const or enum
-    schema: JsonSchema
+class Enum(_DirectWrapper):
+    """`Literal[...]`."""
 
 
 @final
-class Primitive(_Tagged):
-    # `"type": "string"`
-    schema: JsonSchema
+class Const(_DirectWrapper):
+    """`Literal[<one>]`."""
 
 
 @final
-class PrimitiveUnion(_Tagged):
-    # `"type": ["null", "string", "number", "boolean"]`
-    schema: JsonSchema
+class Primitive(_DirectWrapper):
+    """`"type": ~("array" | "object")`."""
 
 
 @final
-class EmptySequence(_Tagged):
-    # `{"maxItems": 0, "minItems": 0, "type": "array"}`
-    schema: JsonSchema
+class PrimitiveUnion(_DirectWrapper):
+    """`"type": ["null", "string", "number", "boolean"]`."""
 
 
 @final
-class Sequence(_Tagged):
-    # `"type": "array"`
-    # may have `items`, `minItems`, `maxItems`
-    schema: JsonSchema
-    items: JsonWrapper | cabc.Sequence[JsonWrapper]
-    min: int
+class EmptySequence(_DirectWrapper):
+    """`{"maxItems": 0, "minItems": 0, "type": "array"}`."""
+
+
+@final
+class NamedSequence(JsonWrapper):
+    """Like a `NamedTuple`, but will used `Annotated` on a regular tuple instead."""
+
+    fields: dict[str, JsonWrapper]
+
+    @classmethod
+    def from_schema(cls, schema: JsonSchema) -> NamedSequence:
+        items = schema.items
+        if isinstance(items, (JsonSchema, bool)):
+            msg = "Use `_from_schema` instead"
+            raise TypeError(msg)
+        return NamedSequence(fields={el.title: _from_schema(el) for el in items}, schema=schema)
+
+
+@final
+class Sequence(JsonWrapper):
+    """`"type": "array"`."""
+
+    items: JsonWrapper
+    min: int = 0
     max: int | None = None
 
     @classmethod
-    def from_schema(cls, schema: JsonSchema) -> Sequence | EmptySequence:
+    def from_schema(cls, schema: JsonSchema) -> Sequence:
         items = schema.items
-        if isinstance(items, bool):
-            if schema.max_items != 0:
-                msg = f"{items=}, in {schema!r}"
-                raise TypeError(msg)
-            return EmptySequence(schema)
-        w_items = (
-            _from_schema(items)
-            if isinstance(items, JsonSchema)
-            else [_from_schema(el) for el in items]
+        if not isinstance(items, JsonSchema):
+            msg = "Use `_from_schema` instead"
+            raise TypeError(msg)
+        return Sequence(
+            items=_from_schema(items), min=schema.min_items, max=schema.max_items, schema=schema
         )
-        return Sequence(schema, w_items, schema.min_items, schema.max_items)
 
 
 type Closed = L["closed"]
@@ -110,10 +170,9 @@ type Closed = L["closed"]
 
 
 @final
-class Object(_Tagged):
-    # `"type": "object"`
-    # may have `properties`, `additionalProperties`, `required`
-    schema: JsonSchema
+class Object(JsonWrapper):
+    """`"type": "object"` AKA a class."""
+
     properties: dict[str, JsonWrapper]
     required: cabc.Sequence[str]
     closed: Closed | None
@@ -121,45 +180,57 @@ class Object(_Tagged):
 
     @classmethod
     def from_schema(cls, schema: JsonSchema) -> Object:
-        props = {k: _from_schema(v) for k, v in schema.properties.items()}
-        req = schema.required
         additional = schema.additional_properties
+        closed = None
+        extra_items = None
         if additional is False:
-            r = Object(schema, props, req, "closed", None)
+            closed = "closed"
         elif additional is not True:
-            r = Object(schema, props, req, None, _from_schema(JsonSchema(**_asdict(additional))))
+            extra_items = _from_schema(convert_json(additional, JsonSchema))
         else:
-            r = Object(schema, props, req, None, Unknown(_EMPTY_SCHEMA))
-        return r
+            extra_items = Unknown()
+        return Object(
+            properties={k: _from_schema(v) for k, v in schema.properties.items()},
+            required=schema.required,
+            closed=closed,
+            extra_items=extra_items,
+            schema=schema,
+        )
 
 
 def _from_schema(schema: JsonSchema) -> JsonWrapper:
     if schema.ref:
-        r = Reference(schema)
+        tp = Reference
     elif schema.any_of:
-        r = Union.from_schema(schema)
-    elif schema.const or schema.enum:
-        r = Literal(schema)
+        tp = Union
+    elif schema.enum:
+        tp = Enum
+    elif schema.const:
+        tp = Const
     elif schema_type := schema.type:
         if schema_type == "object":
-            r = Object.from_schema(schema)
+            tp = Object
         elif schema_type == "array":
-            r = Sequence.from_schema(schema)
+            return _from_schema_array(schema)
         elif not isinstance(schema_type, str):
-            r = PrimitiveUnion(schema)
+            tp = PrimitiveUnion
         else:
-            r = Primitive(schema)
-    elif schema == _EMPTY_SCHEMA:
-        r = Unknown(schema)
+            tp = Primitive
     else:
-        # quite a hassle to remove defaults, leaving just description
-        raw = convert_json(schema, dict[str, Any])
-        if raw.keys() == {"description"}:
-            r = Unknown(schema)
-        else:
-            msg = f"Unexpected schema pattern found: {schema!r}\n{raw!r}"
-            raise NotImplementedError(msg)
-    return r
+        tp = Unknown
+    return tp.from_schema(schema)
+
+
+def _from_schema_array(schema: JsonSchema) -> Sequence | NamedSequence | EmptySequence:
+    items = schema.items
+    if isinstance(items, bool):
+        if schema.max_items != 0:
+            msg = f"{items=}, in {schema!r}"
+            raise TypeError(msg)
+        tp = EmptySequence
+    else:
+        tp = Sequence if isinstance(items, JsonSchema) else NamedSequence
+    return tp.from_schema(schema)
 
 
 def from_definitions(definitions: dict[DefName, JsonSchema]) -> dict[DefName, JsonWrapper]:
