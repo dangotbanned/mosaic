2a7f8bf9d075463bdd5a8fab1bfdc1fefa60f362
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2026-08-26 22:14:13 +0000

    feat: Impl `ChildMatcher`, re-impl `DefsMatcher`
    All are now classes, which makes introspection easier

--- packages/vgplot/spec-python/tools/ir/mlir/scopes.py
+++ packages/vgplot/spec-python/tools/ir/mlir/scopes.py
@@ -8,7 +8,8 @@ Here we convert that into something more optimized for the search itself.
 from __future__ import annotations
 
 import typing
-from collections.abc import Callable, Iterable
+from collections.abc import Callable, Iterable, Iterator
+from itertools import chain
 from typing import Final, Literal as L, Self
 
 from tools.ir.mlir import nodes as mlir
@@ -18,15 +19,24 @@ from tools.models.base import DefName, Entry, IdName
 if typing.TYPE_CHECKING:
     from tools.ir.mlir.definition import Definition
     from tools.ir.mlir.root import Root
-    from tools.models.config import Depth, Filter, IterOver, NamesNodes, Scopes
+    from tools.models.config import Depth, Filter, IterOver, NamesNodes, Nodes, Scopes
 
 type Unused = typing.Any
 type Incomplete = typing.Any
 type DefsEntries = Iterable[Entry[Definition[MLIR]]]
-type DefsMatcherFn = Callable[[Root], DefsEntries]
 type IdMatcher = IdAlways | IdInclude | IdNotExclude
+type DefsMatcher = DefsAlways | DefsIncludeNames | DefsExcludeNames | DefsIncludeNodes | DefsGeneral
+type ChildMatcher = ChildAlways | ChildIncludeNodes
+
+type GroupByIter[T, S] = Iterator[tuple[T, Iterator[S]]]
+"""An iterator with the same shape as [`itertools.groupby`][].
+
+- On each iteration, it pulls a named `Definition` and an iterator over it's matching children.
+- Each `Definition` is guaranteed to have at-least one matching child
+"""
 
 
+# TODO @dangotbanned: `ref`
 class Matcher:
     # NOTE: For this part, some options are:
     # 1. Treat every action as an individual. Do everything sequentially. Use the scope as-is.
@@ -34,12 +44,22 @@ class Matcher:
     #   i. Possibly in combination with others, to decide what kind of "cleanup" would be needed
     # 3. Transform `Scopes` into a "compiled" representation
     #   i. Calling the compiled version will be a single call, instead of checking 19 different conditions
-    __slots__ = ("id", "matching_definitions", "over", "ref_follow_depth", "todo_child")
+    __slots__ = ("child", "definition", "id", "over", "ref_follow_depth")
     id: IdMatcher
-    matching_definitions: DefsMatcherFn
-    todo_child: Incomplete
+    definition: DefsMatcher
+    child: ChildMatcher
     over: IterOver
     ref_follow_depth: Depth
+
+    def matching_definitions(self, root: Root) -> DefsEntries:
+        return self.definition.iter_defs(root)
+
+    def matching_children(self, root: Root) -> GroupByIter[Entry[Definition[MLIR]], MLIR]:
+        return self.child.iter_children(self.matching_definitions(root))
+
+    def matching_descendants(self, root: Root) -> Incomplete:
+        msg = "TODO @dangotbanned: Move things from `actions.py` to here"
+        raise NotImplementedError(msg)
 
     @classmethod
     def from_scopes(cls, scopes: Scopes) -> Self:
@@ -50,43 +70,22 @@ class Matcher:
         else:
             self.id = IdInclude(incl_id - exclude.id) if exclude.id else IdInclude(incl_id)
 
-        self.matching_definitions = _into_defs_matcher(include, exclude)
+        self.definition = _into_defs_matcher(include, exclude)
         self.over = scopes.over
         self.ref_follow_depth = scopes.ref_follow_depth
 
-        if include.child or include.ref:
-            msg = f"include.{('child' if include.child else 'ref')} is not yet implemented, got:\n{include!r}"
-            raise NotImplementedError(msg)
-        if exclude.child or exclude.ref:
-            msg = f"exclude.{('child' if exclude.child else 'ref')} is not yet implemented, got:\n{exclude!r}"
+        if include.child or exclude.child:
+            self.child = ChildIncludeNodes(include.child, exclude.child)
+        else:
+            self.child = _CHILD_ALWAYS
+        if include.ref or exclude.ref:
+            msg = f"{('include' if include.ref else 'exclude')}.ref is not yet implemented, got:\n{(include.ref or exclude.ref)!r}"
             raise NotImplementedError(msg)
 
-        # TODO @dangotbanned: `child``
-        # TODO @dangotbanned: `ref`
         return self
 
-    def search_eager(self, root: Root) -> None:
-        if self.id.matches(root.id):
-            if self.over == "descendants":
-                for name, node in self.matching_definitions(root):
-                    # yields `node` first
-                    node.inner.iter_descendants()
-                    msg = f"TODO: successful match on defs ({self.over=}): {name!r}, {node.inner!r}"
-                    raise NotImplementedError(msg)
 
-            elif self.over == "children":
-                for name, node in self.matching_definitions(root):
-                    # does not yield `node`
-                    node.inner.iter_children()
-                    msg = f"TODO: successful match on defs ({self.over=}): {name!r}, {node.inner!r}"
-                    raise NotImplementedError(msg)
-            else:
-                for name, node in self.matching_definitions(root):
-                    msg = f"TODO: successful match on defs ({self.over=}): {name!r}, {node.inner!r}"
-                    raise NotImplementedError(msg)
-
-
-def _into_defs_matcher(include: Filter, exclude: Filter) -> DefsMatcherFn:
+def _into_defs_matcher(include: Filter, exclude: Filter) -> DefsMatcher:
     """Transform scope filters into a generator function, yielding matching definitions.
 
     Pretty aggressively optimized, as there are cheap solutions for how I plan to use the syntax.
@@ -101,63 +100,137 @@ def _into_defs_matcher(include: Filter, exclude: Filter) -> DefsMatcherFn:
 
     match (len_incl_names, len_incl_nodes, len_excl_names, len_excl_nodes):
         case (0, 0, 0, 0):
-            return _no_predicate
+            return _DEFS_ALWAYS
         case (_, 0, 0, 0):
-            return _include_names(incl_defs.names)
+            return DefsIncludeNames(incl_defs.names)
         case (0, 0, _, 0):
-            return _exclude_names(excl_defs.names)
+            return DefsExcludeNames(excl_defs.names)
         case (0, _, 0, _):
-            return _only_types(incl_defs, excl_defs)
+            return DefsIncludeNodes(incl_defs, excl_defs)
         case _:
-            return _unoptimized(incl_defs, excl_defs)
+            return DefsGeneral(incl_defs, excl_defs)
 
 
-def _no_predicate(root: Root, /) -> DefsEntries:
-    return root.def_items()
+def _ordered_args(iterable: Iterable[str], /) -> str:
+    """Display as a variadic argument list."""
+    items = sorted(iterable)
+    if len(items) == 1:
+        return f"({items[0]!r})"
+    if items:
+        return f"{tuple(items)!r}"
+    return "()"
 
 
-def _include_names(names: frozenset[DefName], /) -> DefsMatcherFn:
-    def _(root: Root) -> DefsEntries:
-        return root.iter_defs_by_name(names)
+class ChildAlways:
+    __slots__ = ()
 
-    return _
+    def __repr__(self) -> str:
+        return "child.always()"
+
+    def iter_children(self, entries: DefsEntries, /) -> GroupByIter[Entry[Definition[MLIR]], MLIR]:
+        for name, node in entries:
+            it = node.inner.iter_children()
+            if first := next(it, None):
+                yield (name, node), chain((first,), it)
 
 
-def _exclude_names(names: frozenset[DefName], /) -> DefsMatcherFn:
-    in_exclude = names.__contains__
+class ChildIncludeNodes:
+    __slots__ = ("types",)
 
-    def _(root: Root) -> DefsEntries:
+    def __repr__(self) -> str:
+        return f"child.node.is_in{_ordered_args(tp.__name__ for tp in self.types)}"
+
+    def __init__(self, incl: Nodes, excl: Nodes, /) -> None:
+        self.types: tuple[type[MLIR], ...] = _convert_nodes(incl, excl)
+
+    def iter_children(self, entries: DefsEntries, /) -> GroupByIter[Entry[Definition[MLIR]], MLIR]:
+        types = self.types
+        for name, node in entries:
+            it = (child for child in node.inner.iter_children() if isinstance(child, types))
+            if first := next(it, None):
+                yield (name, node), chain((first,), it)
+
+
+class DefsAlways:
+    __slots__ = ()
+
+    def __repr__(self) -> str:
+        return "definition.always()"
+
+    def iter_defs(self, root: Root, /) -> DefsEntries:
+        return root.def_items()
+
+
+class DefsIncludeNames:
+    __slots__ = ("names",)
+
+    def __repr__(self) -> str:
+        return f"definition.name.is_in{_ordered_args(self.names)}"
+
+    def __init__(self, names: frozenset[DefName], /) -> None:
+        self.names: frozenset[DefName] = names
+
+    def iter_defs(self, root: Root, /) -> DefsEntries:
+        return root.iter_defs_by_name(self.names)
+
+
+class DefsExcludeNames:
+    __slots__ = ("_in_exclude", "names")
+
+    def __repr__(self) -> str:
+        return f"definition.name.is_not_in{_ordered_args(self.names)}"
+
+    def __init__(self, names: frozenset[DefName], /) -> None:
+        self.names: frozenset[DefName] = names
+        self._in_exclude: Callable[[DefName], bool] = self.names.__contains__
+
+    def iter_defs(self, root: Root, /) -> DefsEntries:
+        in_exclude = self._in_exclude
         yield from ((name, node) for name, node in root.def_items() if not in_exclude(name))
 
-    return _
 
+class DefsIncludeNodes:
+    __slots__ = ("types",)
 
-def _only_types(incl_defs: NamesNodes, excl_defs: NamesNodes, /) -> DefsMatcherFn:
-    types = _convert_nodes(incl_defs, excl_defs)
+    def __repr__(self) -> str:
+        return f"definition.node.is_in{_ordered_args(tp.__name__ for tp in self.types)}"
 
-    def _(root: Root) -> DefsEntries:
+    def __init__(self, incl_defs: NamesNodes, excl_defs: NamesNodes, /) -> None:
+        self.types: tuple[type[MLIR], ...] = _convert_nodes(incl_defs, excl_defs)
+
+    def iter_defs(self, root: Root, /) -> DefsEntries:
+        types = self.types
         yield from (
             (name, node) for name, node in root.def_items() if isinstance(node.inner, types)
         )
 
-    return _
 
+class DefsGeneral:
+    __slots__ = ("exclude_names", "include_names", "types")
 
-def _unoptimized(incl_defs: NamesNodes, excl_defs: NamesNodes, /) -> DefsMatcherFn:
-    types = _convert_nodes(incl_defs, excl_defs)
+    def __repr__(self) -> str:
+        s = f"node.is_in{_ordered_args(tp.__name__ for tp in self.types)}"
+        if include := self.include_names:
+            s = f"{s} & name.is_in{_ordered_args(include)}"
+        if exclude := self.exclude_names:
+            s = f"{s} & name.is_not_in{_ordered_args(exclude)}"
+        return f"definition.({s})"
 
-    def _(root: Root) -> DefsEntries:
+    def __init__(self, incl_defs: NamesNodes, excl_defs: NamesNodes, /) -> None:
+        self.types: tuple[type[MLIR], ...] = _convert_nodes(incl_defs, excl_defs)
+        self.include_names: frozenset[DefName] = incl_defs.names
+        self.exclude_names: frozenset[DefName] = excl_defs.names
+
+    def iter_defs(self, root: Root, /) -> DefsEntries:
         names = root.def_names()
-        if incl_defs.names:
-            names = names & incl_defs.names
-        if excl_defs.names:
-            names = names - excl_defs.names
-        include_name = names.__contains__
-        for name, node in root.def_items():
-            if include_name(name) and isinstance(node.inner, types):
+        if self.include_names:
+            names = names & self.include_names
+        if self.exclude_names:
+            names = names - self.exclude_names
+        types = self.types
+        for name, node in root.iter_defs_by_name(names):
+            if isinstance(node.inner, types):
                 yield name, node
-
-    return _
 
 
 _MLIR_TYPES = frozenset(
@@ -185,8 +258,8 @@ _MLIR_TYPES = frozenset(
 )
 
 
-def _convert_nodes(incl_defs: NamesNodes, excl_defs: NamesNodes, /) -> tuple[type[MLIR], ...]:
-    nodes = (incl_defs.nodes or _MLIR_TYPES).difference(excl_defs.nodes)
+def _convert_nodes(incl: NamesNodes | Nodes, excl: NamesNodes | Nodes, /) -> tuple[type[MLIR], ...]:
+    nodes = (incl.nodes or _MLIR_TYPES).difference(excl.nodes)
     return tuple[type[MLIR], ...](getattr(mlir, name) for name in nodes)
 
 
@@ -194,7 +267,7 @@ class IdInclude:
     __slots__ = ("names",)
 
     def __repr__(self) -> str:
-        return f"id.is_in{tuple(sorted(self.names))!r}"
+        return f"id.is_in{_ordered_args(self.names)}"
 
     def __init__(self, names: frozenset[IdName], /) -> None:
         self.names: frozenset[IdName] = names
@@ -207,7 +280,7 @@ class IdNotExclude:
     __slots__ = ("_in_exclude", "names")
 
     def __repr__(self) -> str:
-        return f"id.is_not_in{tuple(sorted(self.names))!r}"
+        return f"id.is_not_in{_ordered_args(self.names)}"
 
     def __init__(self, names: frozenset[IdName], /) -> None:
         self.names: frozenset[IdName] = names
@@ -228,3 +301,5 @@ class IdAlways:
 
 
 _ID_ALWAYS: Final = IdAlways()
+_DEFS_ALWAYS: Final = DefsAlways()
+_CHILD_ALWAYS: Final = ChildAlways()
