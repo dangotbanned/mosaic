6e812cca827d96dbfe0a8e2df784ea5dc95fe075
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2026-09-14 16:23:56 +0000

    test: add `test_peps` directory

---
+++ packages/vgplot/spec-python/tests/test_peps/__init__.py
@@ -0,0 +1,7 @@
+"""Handwritten test suite, inspired by related [Typing PEPs].
+
+## Note
+*PEP* stands for Python Enhancement Proposal.
+
+[Typing PEPs]: https://peps.python.org/topic/typing
+"""
---
+++ packages/vgplot/spec-python/tests/test_peps/test_pep_589.py
@@ -0,0 +1,4 @@
+"""[PEP 589] - TypedDict: Type Hints for Dictionaries with a Fixed Set of Keys.
+
+[PEP 589]: https://peps.python.org/pep-0589/
+"""
