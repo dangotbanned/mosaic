7dbf1564d7b91df4e3d0cab291c630b448d43f43
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2026-09-14 16:19:12 +0000

    docs: Explain `test_examples`

---
+++ packages/vgplot/spec-python/tests/test_examples/__init__.py
@@ -0,0 +1,11 @@
+"""Test suite derived from [specs/yaml][1].
+
+Each test module corresponds to a single spec, for example:
+
+[aeromagnetic-survey.yaml][2] -> `test_aeromagnetic_survey.py`
+
+**All of these tests are generated** via `pnpm generate:examples`.
+
+[1]: https://github.com/uwdata/mosaic/tree/main/specs/yaml
+[2]: https://github.com/uwdata/mosaic/blob/main/specs/yaml/aeromagnetic-survey.yaml
+"""
