462fc4ceeea4372c95083d6bad96ef0384ef8e4f
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2026-09-14 18:32:33 +0000

    test: Cover inheritance

--- packages/vgplot/spec-python/tests/test_peps/test_pep_589.py
+++ packages/vgplot/spec-python/tests/test_peps/test_pep_589.py
@@ -8,6 +8,10 @@ from __future__ import annotations
 from typing_extensions import assert_type
 
 import mosaic_spec as ms
+from mosaic_spec._gen.transform import AggregateOptions, WindowOptions
+from mosaic_spec._typing_compat import NotRequired, TypeAliasType
+
+Arg = TypeAliasType("Arg", ms.ParamRef | bool | float | str)
 
 
 def test_abstract() -> None:
@@ -44,3 +48,37 @@ def test_using_typed_dict_types() -> None:
         brush={"fill": "red", "fill_opacity": 0.8, "stroke": "green"},
         channels=("x",),
     )
+
+
+def test_inheritance() -> None:
+    class NullCount1(AggregateOptions):
+        null_count: Arg | tuple[Arg]
+
+    class NullCount2(WindowOptions):
+        null_count: Arg | tuple[Arg]
+        distinct: NotRequired[bool]
+
+    def accepts_null_count(agg: NullCount1) -> None: ...
+
+    accepts_null_count(NullCount1(null_count="a1"))
+    accepts_null_count(NullCount2(null_count="a2"))
+    accepts_null_count({"distinct": True, "null_count": ("b",)})
+
+
+def test_inheritance_multi() -> None:
+    """Covering multi-inheritance indirectly via intersection types.
+
+    Almost every public `TypedDict` uses `closed=True`, which makes inheritance mostly useless.
+    """
+    parquet = ms.DataParquet(type="parquet", file="path/to/file.parquet")
+    param = ms.ParamRef("$table")
+
+    # Ok
+    ms.Area(mark="area", data={"source": param})
+    ms.spec.Area(mark="area", params={"a": 1})
+    ms.spec.Area(mark="area", data={"named": parquet})
+
+    # Fail
+    ms.spec.Area(mark="area", data={"source": param})  # ty: ignore[invalid-argument-type] # pyrefly: ignore[bad-assignment]  # pyright: ignore[reportArgumentType]
+    ms.Area(mark="area", params={"a": 1})  # ty: ignore[missing-typed-dict-key, invalid-key] # pyrefly: ignore[missing-argument, unexpected-keyword]  # pyright: ignore[reportCallIssue]
+    ms.Area(mark="area", data={"named": parquet})  # ty: ignore[missing-typed-dict-key, invalid-key] # pyrefly: ignore[bad-argument-type]  # pyright: ignore[reportArgumentType, reportCallIssue]
