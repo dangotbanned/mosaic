70f7072a85a2cd39a46f3a51a74fbb0ae7807a2d
dangotbanned <125183946+dangotbanned@users.noreply.github.com>
2026-09-18 14:55:27 +0000

    test(PEP 655): Add `functools.partial` coverage
    I really want to be notified if `pyrefly` fixes this

--- packages/vgplot/spec-python/tests/test_peps/test_pep_655.py
+++ packages/vgplot/spec-python/tests/test_peps/test_pep_655.py
@@ -5,18 +5,68 @@
 
 from __future__ import annotations
 
-from typing import Literal as L
+from functools import partial
+from typing import TYPE_CHECKING, Literal as L
 
 import pytest
 
 import mosaic_spec as ms
+from mosaic_spec._typing_compat import ParamSpec, TypeVar, assert_type
+
+if TYPE_CHECKING:
+    from collections.abc import Callable
 
 
-def test_missing_required() -> None:
+def test_required_missing() -> None:
     ms.Menu()  # ty: ignore[missing-typed-dict-key] # pyrefly: ignore [no-matching-overload] # pyright: ignore[reportCallIssue]
     ms.Menu({})  # ty: ignore[missing-typed-dict-key] # pyrefly: ignore [bad-typed-dict-key] # pyright: ignore[reportArgumentType]
     ms.Menu(input="menu")
     ms.Menu({"input": "menu"})
+
+
+P = ParamSpec("P")
+R = TypeVar("R")
+
+
+def into_callable(cb: Callable[P, R], /) -> Callable[P, R]:
+    """https://typing.python.org/en/latest/spec/constructors.html#converting-a-constructor-to-callable"""
+    return cb
+
+
+def test_required_partial_direct() -> None:
+    # NOTE: No type checker warns about this, yet?
+    table = partial(ms.Table, input="table")
+    bad = table()
+    with pytest.raises(KeyError):
+        bad["source"]
+
+
+def test_required_partial_indirect() -> None:
+    """This came from experimenting with a convenient syntax for discrimnator fields.
+
+    ## Notes
+    - `ty` fully understands
+    - `pyright` isn't happy with `into_callable` mixing with overloads
+        - But understands the use of `partial` afterwards
+    - `pyrefly` treats everything as `Any`, and therefore fails
+    """
+    cb = into_callable(ms.Table)  # pyright: ignore[reportArgumentType]
+    table = partial(cb, input="table")
+
+    bad_1 = table()  # ty: ignore[no-matching-overload]  # pyright: ignore[reportCallIssue]
+    _bad_2 = table(height=10)  # ty: ignore[no-matching-overload]  # pyright: ignore[reportCallIssue]
+
+    good_1 = table(source="somewhere")
+    good_2 = table(height=10, source="somewhere")
+
+    # NOTE: `pyrefly` is wrong
+    # if this becomes `unused-ignore` then using `functools.partial` to build an API is viable
+    assert_type(good_1, ms.Table)  # pyrefly: ignore [assert-type]
+    assert_type(good_2, ms.Table)  # pyrefly: ignore [assert-type,bad-typed-dict-key]
+
+    assert good_1["source"] == "somewhere"
+    with pytest.raises(KeyError):
+        bad_1["source"]
 
 
 @pytest.mark.xfail(reason="https://github.com/python/cpython/issues/97727")
